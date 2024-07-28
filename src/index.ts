75db7a7cfdbbf311780b4445be5943bfbc980eaa
Human Resource <khalani034@naturalsrs.com>
2024-07-29 01:31:16 +0800

    fix Readable Stream lock error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { convertToEML, covertToJSON } from "./MessageParser";
-import { sendWebhook } from "./Webhook";
-import { ForwardableEmailMessage, Headers } from '@cloudflare/workers-types'
+import { convertToEML, convertToJSON } from "./utils/email"
+import { sendWebhook } from "./utils/webhook"
+import { ForwardableEmailMessage } from '@cloudflare/workers-types'
+
+export interface Env {
+    WEBHOOK_URL: string
+}
 
 export default {
     /**
@@ -10,29 +14,38 @@ export default {
      * @param ctx - Execution context.
      * @returns A promise resolving to any result.
      */
-    async email(message: ForwardableEmailMessage): Promise<any> {
-        const emailStream = message.raw;
-        const webhookUrl = '<WEBHOOK_URL>';
-
+    async email(message: ForwardableEmailMessage, env: any, ctx?:any): Promise<void> {
         try {
-            const dataEML = await convertToEML(emailStream);
-            const sendEML = await sendWebhook(webhookUrl, dataEML);
-            if (sendEML.ok) {
-                console.log('Email EML forwarded successfully');
+            // Get Uint8Array raw message
+            const rawMessage = message.raw
+
+            // Get webhook url from wrangler env [vars] => wrangler.toml
+            const webhookUrl = env.WEBHOOK_URL
+            
+            // Convert raw message to readable string
+            const dataEML = await convertToEML(rawMessage)
+
+            // Convert readable eml string to JSON
+            const dataJSON = await convertToJSON(dataEML)
+
+            // Send JSON data to webhook
+            const sendJSON = await sendWebhook(webhookUrl, dataJSON, 'application/json')
+            if (sendJSON.ok) {
+                console.log('Email JSON forwarded successfully')
             } else {
-                console.error('Failed to forward email EML', sendEML.statusText);
+                console.error('Failed to forward email JSON', sendJSON.statusText)
             }
 
-            const dataJSON = await covertToJSON(emailStream);
-            const sendJSON = await sendWebhook(webhookUrl, dataJSON);
-            if (sendJSON.ok) {
-                console.log('Email JSON forwarded successfully');
+            // Send EML data to webhook
+            const sendEML = await sendWebhook(webhookUrl, dataEML, 'text/plain')
+            if (sendEML.ok) {
+                console.log('Email EML forwarded successfully')
             } else {
-                console.error('Failed to forward email JSON', sendJSON.statusText);
+                console.error('Failed to forward email EML', sendEML.statusText)
             }
         } catch (error) {
-            console.error("Error processing email:", error);
-            throw new Error("Failed to process and forward email");
+            console.error("Error processing email:", error)
+            throw new Error("Failed to process and forward email")
         }
     }
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>
